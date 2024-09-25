3261aa5657212ee7857e7f7eeab7bed92e9b35cd
daniel <danilrodionov8665@gmail.com>
2024-09-25 10:36:07 +0300

    пр 3

--- pr3.py
+++ pr3.py
@@ -1,23 +1,10 @@
 a = int(input('1 число: '))
 b = int(input('2 число: '))
 c = int(input('3 число: '))
-if a < b and a < c:
+if a > 0 and a < 4:
     print(a)
-elif b < a and b < c:
+if b > 0 and b < 4:
     print(b)
-elif c < a and c < b:
+if c > 0 and c < 4:
     print(c)
-elif a < b and a == c:
-    print(a)
-elif b < a and b == c:
-    print(b)
-elif c < a and c == b:
-    print(c)
-elif a == b and a < c:
-    print(a)
-elif b == a and b < c:
-    print(b)
-elif c == a and c < b:
-    print(c)
-elif a == b and b == c:
-    print(a)
+
