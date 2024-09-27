a174bbb8ba811440029d270cf47244ccc88653b9
daniel <danilrodionov8665@gmail.com>
2024-09-27 10:21:52 +0300

    пр 3 зад 2

--- pr3.2.py
+++ pr3.2.py
@@ -1,5 +1,20 @@
+summ = lambda x, y, z: (x + y + z)
+print(summ(int(input()), int(input()), int(input()))) # 1
+
+
 def s(a, b):
     return 1/2 * a * b
 
 
-print(s(int(input()), int(input())))
+print(s(int(input()), int(input()))) # 2
+
+
+def less(a, b, c):
+    li = []
+    li.append(a)
+    li.append(b)
+    li.append(c)
+    return min(li)
+
+
+print(less(int(input()), int(input()), int(input()))) # 5
