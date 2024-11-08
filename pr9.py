93f4ce6865fd0591cfdf3f41eb03f9dc6f6f891b
Daniel <danilrodionov8665@gmail.com>
2024-11-08 10:57:52 +0300

    пр 9 вар 2

---
+++ pr9.py
@@ -0,0 +1,31 @@
+n = int(input())
+matr = []
+res = []
+resf = 0
+f = 0
+for i in range(n):
+    s = input().split()
+    s = [int(x) for x in s]
+    matr.append(s)
+for k in range(len(matr) - 1):
+    if sum(matr[k]) == sum(matr[k + 1]):
+        f = 1
+    else:
+        f = 0
+if f == 1:
+    for i in range(len(matr)):
+        st = []
+        for j in range(len(matr[i])):
+            st.append(matr[j][i])
+        res.append(st)
+    for i in range(len(res) - 1):
+        if sum(res[i]) == sum(res[i + 1]):
+            resf = 1
+        else:
+            resf = 0
+    if resf == 1:
+        print('Квадрат магический')
+    else:
+        print('Квадрат не магический')
+else:
+    print('Квадрат не магический')
